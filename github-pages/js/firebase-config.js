e9a835d1c79afdf4d6bf29e2d911042fe91e1416
himawariclubyawata-sys <himawari.club.yawata@gmail.com>
2026-07-17 00:07:37 +0900

    Delete github-pages directory

--- github-pages/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDcs5UX1kN04XrJr1qoKdaviJTwq0Sn_GA",
-  authDomain: "himawari-club.firebaseapp.com",
-  projectId: "himawari-club",
-  storageBucket: "himawari-club.firebasestorage.app",
-  messagingSenderId: "630304105725",
-  appId: "1:630304105725:web:4a012e4d16ca119fda0a94"
-};
